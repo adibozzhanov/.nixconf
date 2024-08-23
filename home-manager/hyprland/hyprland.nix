0875c888772d88bf242360148c73bb9a0a0c3d24
Adi Bozzhanov <bozzhanov.at@gmail.com>
2024-08-23 21:57:25 +0100

    Home rebuild: 2024-08-23 21:57 : id 88 -> /nix/store/93bc25fcahlhyvl1ifvwibw80rnprk4d-home-manager-generation

--- home-manager/hyprland/hyprland.nix
+++ home-manager/hyprland/hyprland.nix
@@ -14,23 +14,17 @@
   wayland.windowManager.hyprland.enable = true;
 
   home.packages = with pkgs; [
-    hyprland-protocols # this some recommended thing for other stuff...
-    swww # wallpaper engine
-    waypaper # wallpapers gui
+    hyprland-protocols          # this some recommended thing for other stuff...
+    swww                        # wallpaper engine
+    waypaper                    # wallpapers gui
     xdg-desktop-portal-hyprland # some recommended thing for video streaming
-    hyprshot # screenshots
-    swaynotificationcenter # notifications
-    qt5.qtwayland # to be able to open qt windows
+    hyprshot                    # screenshots
+    swaynotificationcenter      # notifications
+    qt5.qtwayland               # to be able to open qt windows
     libsForQt5.polkit-kde-agent # authentication prompt for apps that are not opened in terminal
   ];
   
-
   wayland.windowManager.hyprland.settings = {    
-
-    windowrulev2 = [
-      "suppressevent maximize, class:.*"
-    ];
-
     input = {
       kb_layout = "us";
       follow_mouse = "1";
