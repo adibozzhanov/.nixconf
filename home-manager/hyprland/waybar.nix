b5e813eb81929fca677e962763af915aee7a5a57
Adi Bozzhanov <bozzhanov.at@gmail.com>
2024-08-25 08:59:52 +0100

    Home rebuild: 2024-08-25 08:59 : id 144 -> /nix/store/kgx4a650rp2frf0aqdifa84r9iyr79xy-home-manager-generation

--- home-manager/hyprland/waybar.nix
+++ home-manager/hyprland/waybar.nix
@@ -1,6 +1,23 @@
 {
   programs.waybar = {
     enable = true;
-    systemd.enable = true;   
-  };
+    systemd.enable = true;
+    settings = [{
+      height = 30;
+      layer = "top";
+      modules-right = [
+        "battery"
+        "network"        
+      ];
+
+      modules-center = [
+        "clock"
+      ];
+
+      modules-left = [
+        "cpu"
+        "memory"
+      ];      
+    }];
+  };  
 }
