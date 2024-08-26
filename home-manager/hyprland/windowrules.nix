9397ca1bf5a183eceec84167fe2717844740b33c
Adi Bozzhanov <bozzhanov.at@gmail.com>
2024-08-26 21:57:23 +0100

    Home rebuild: 2024-08-26 21:57 : id 168 -> /nix/store/cfvhxpdm6i8na044v5s5mvsczbnw1v1k-home-manager-generation

--- home-manager/hyprland/windowrules.nix
+++ home-manager/hyprland/windowrules.nix
@@ -2,11 +2,13 @@
   wayland.windowManager.hyprland.settings = {
     windowrulev2 = [
       "suppressevent maximize, class:.*"
+      
       "opacity 0.9 0.8, class:^(kitty)$"
       "opacity 0.9 0.8, class:^(Spotify)$"
       "opacity 0.95 0.8, class:^(org.telegram.desktop)$"
       "opacity 1 0.8, class:^(firefox)$"
       "float, class: ^(waypaper)$"
+      "float, class: ^(.blueman-manager-wrapped)$"
       "idleinhibit fullscreen, class:^(*)$"
       "idleinhibit fullscreen, title:^(*)$"
       "idleinhibit fullscreen, fullscreen:1"
