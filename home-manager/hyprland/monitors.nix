f7eb2a8c49b76f00f78d1b85644740d98f1adad0
Adi Bozzhanov <bozzhanov.at@gmail.com>
2024-08-26 20:54:57 +0100

    Home rebuild: 2024-08-26 20:54 : id 150 -> /nix/store/ls1psmhp7l46m7kdw0dwi7apjp6v6cy4-home-manager-generation

--- home-manager/hyprland/monitors.nix
+++ home-manager/hyprland/monitors.nix
@@ -3,7 +3,7 @@
     monitor = [
       ",preferred, auto, auto"
       "desc: JRY DX238A1, highrr, auto-right, auto"
-      "desc: Dell Inc. DELL SE2219H 7LYJPR2, auto, auto-right, auto"      
+      "desc: Dell Inc. DELL SE2219H 7LYJPR2, preferred, auto-right, auto"
     ];
   };
 }
