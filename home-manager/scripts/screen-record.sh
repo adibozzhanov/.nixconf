b84e0f83cbd9fdc7e484c67d3e8eaef3827df32b
Adi Bozzhanov <bozzhanov.at@gmail.com>
2024-08-26 21:34:53 +0100

    scripts update

--- home-manager/scripts/screen-record.sh
+++ home-manager/scripts/screen-record.sh
@@ -4,11 +4,14 @@ CLIPFILE="/home/bzv/recordings/rec-$(date +%F_%T).mp4"
 
 pkill "wf-recorder" \
     || (wf-recorder \
-      -c libx264 \
-      -f $CLIPFILE \
-      -g "$(slurp && notify-send 'Recording Started')" \
-      && notify-send \
-       --action="Open file"\
-       "Recording Saved"\
-    | if [ -p /dev/stdin ] && [ "$(cat)" = "0" ]; then nautilus -s $CLIPFILE; fi \
-    )
+	   -c libx264 \
+	   -r 60 \
+	   -x yuv420p \
+	   -D \
+	   -f $CLIPFILE \
+	   -g "$(slurp && notify-send 'Recording Started')" \
+	    && notify-send \
+		   --action="Open file"\
+		   "Recording Saved"\
+		| if [ -p /dev/stdin ] && [ "$(cat)" = "0" ]; then nautilus -s $CLIPFILE; fi \
+	   )
