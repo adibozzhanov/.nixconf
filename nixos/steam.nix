b6c4293b55f00671089bfcb38e890e50faa5420e
Adi Bozzhanov <bozzhanov.at@gmail.com>
2024-08-26 23:10:09 +0100

    Home rebuild: 2024-08-26 23:07 : id 181 -> /nix/store/mv322cqd9dqbx3ihvvv717mn677ndqdq-home-manager-generation

--- nixos/steam.nix
+++ nixos/steam.nix
@@ -4,5 +4,12 @@
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
     localNetworkGameTransfers.openFirewall = true;
+    package = pkgs.steam.override {
+      extraPkgs = pkgs: with pkgs; [
+        libkrb5
+        keyutils
+
+      ];
+    };
   };
 }
