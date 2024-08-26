0caeab10654d4fa6be99685a572aa9e746a3d841
Adi Bozzhanov <bozzhanov.at@gmail.com>
2024-08-26 20:55:32 +0100

    Home rebuild: 2024-08-26 20:55 : id 151 -> /nix/store/vpzfq1gpnx9djdl1jndai9kzbbaa1aq5-home-manager-generation

--- home-manager/hyprland/monitors.nix
+++ home-manager/hyprland/monitors.nix
@@ -2,7 +2,7 @@
   wayland.windowManager.hyprland.settings = {
     monitor = [
       ",preferred, auto, auto"
-      "desc: JRY DX238A1, highrr, auto-right, auto"
+      "desc: JRY DX238A1, highrr, 1280x0, auto"
       "desc: Dell Inc. DELL SE2219H 7LYJPR2, preferred, auto-right, auto"
     ];
   };
