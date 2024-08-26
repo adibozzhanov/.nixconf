5a3e8dbcb476c01d0db5190126a18a520ebe0912
Adi Bozzhanov <bozzhanov.at@gmail.com>
2024-08-27 00:35:39 +0100

    NixOs rebuild: 21 current  2024-08-27 00:23:54  24.05.20240822.797f7dc   6.6.46                          *

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    nixos-hardware.url = "github:NixOS/nixos-hardware/master"
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
   outputs = { nixpkgs, home-manager, ... }:
