e7bb1c72f34e04a340a9472bdf134845749eac2e
Adi Bozzhanov <bozzhanov.at@gmail.com>
2024-08-27 00:20:32 +0100

    Home rebuild: 2024-08-27 00:20 : id 193 -> /nix/store/jkf813j5b0529241yd26bkj63nahd9nw-home-manager-generation

--- home-manager/hyprland/monitors.nix
+++ home-manager/hyprland/monitors.nix
@@ -1,6 +1,9 @@
 {
   wayland.windowManager.hyprland.settings = {
-    source = "~/.config/hypr/monitors.conf";
+    source = [
+      "~/.config/hypr/monitors.conf"
+      "~/.config/hypr/workspaces.conf"
+    ];
     monitor = [
       ",preferred, auto, auto"
     ];
