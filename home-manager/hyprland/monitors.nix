fe6b8adc6a0dc6c486abd704eeeeecbbbd788569
Adi Bozzhanov <bozzhanov.at@gmail.com>
2024-08-27 00:18:30 +0100

    Home rebuild: 2024-08-27 00:18 : id 192 -> /nix/store/32w51bsmv6k3d5s7bl65scw1hx3ynqi4-home-manager-generation

--- home-manager/hyprland/monitors.nix
+++ home-manager/hyprland/monitors.nix
@@ -3,9 +3,6 @@
     source = "~/.config/hypr/monitors.conf";
     monitor = [
       ",preferred, auto, auto"
-      "eDP-1, highrr, 0x0, auto, bitdepth, 10"
-      "desc:JRY DX238A1, highrr, 1280x0, auto, bitdepth, 10"
-      "desc:Dell Inc. DELL SE2219H 7LYJPR2, preferred, 3200x0, auto, bitdepth, 10"
     ];
   };
 }
