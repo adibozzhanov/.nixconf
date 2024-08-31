83cb7936cb4a68f870c0a23546754543554362b3
Adi Bozzhanov <bozzhanov.at@gmail.com>
2024-08-31 18:44:55 +0100

    Home rebuild: 2024-08-31 18:44 : id 220 -> /nix/store/5if696y72cl1vifrjz1iznsxz8s59ny5-home-manager-generation

--- home-manager/hyprland/waybar.nix
+++ home-manager/hyprland/waybar.nix
@@ -20,7 +20,14 @@
 
     style =
       ''
-
+* {
+    border: none;
+    border-radius: 0;
+    /* `otf-font-awesome` is required to be installed for icons */
+    font-family: JetBrains Mono;
+    font-weight: bold; 
+    min-height: 20px;
+}     
       '';   
   };  
 }
