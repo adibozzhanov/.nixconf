727676974b8ba9afea6fb78bf63a628911f46bcc
Adi Bozzhanov <bozzhanov.at@gmail.com>
2024-09-01 00:35:44 +0100

    Home rebuild: 2024-09-01 00:35 : id 292 -> /nix/store/fak8fcvj81xflji2fn6ycs30syg3x3cd-home-manager-generation

--- home-manager/hyprland/startup.nix
+++ home-manager/hyprland/startup.nix
@@ -2,6 +2,7 @@
   wayland.windowManager.hyprland.settings = {
     exec-once = [
       "swaync"
+      "nm-applet"
       "systemctl --user start plasma-polkit-agent"
       "chmod +x ~/.nixconf/home-manager/scripts/*"
       "~/.nixconf/home-manager/scripts/wallpaper.sh"
