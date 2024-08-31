d5ef4f531f8f31a844c07467359f92f9d17a8fd5
Adi Bozzhanov <bozzhanov.at@gmail.com>
2024-08-31 19:39:13 +0100

    Home rebuild: 2024-08-31 19:39 : id 251 -> /nix/store/493lhbss6ccsa1njw8v375q1dnrr2wvm-home-manager-generation

--- home-manager/hyprland/waybar.nix
+++ home-manager/hyprland/waybar.nix
@@ -18,6 +18,17 @@
         "temperature"
       ];
 
+      battery = {
+        states = {
+          warning = 30;
+          critical = 15;
+        };
+        format = "{capacity}% {icon}";
+        format-charging = "{capacity}% ";
+        format-plugged = "{capacity}% ";
+        format-alt = "{time} {icon}";
+        format-icons = ["" "" "" "" "" "" "" "" "" ""];
+      };
       
       "cpu" = {
         interval = 2;
