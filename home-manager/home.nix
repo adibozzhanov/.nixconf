0fbc88bc1e79c9f20f6548fbedde65f1be245b7f
Adi Bozzhanov <bozzhanov.at@gmail.com>
2024-09-01 01:23:35 +0100

    Home rebuild: 2024-09-01 01:15 : id 307 -> /nix/store/j79yb1q7cmnd8c82w865r373d0b3kk29-home-manager-generation

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -14,6 +14,27 @@
     };
   };
 
+  home = {
+    username = "bzv";
+    homeDirectory = "/home/bzv";
+    file = {
+      "screenshots/.keep".source = builtins.toFile "keep" "";
+      "code/.keep".source = builtins.toFile "keep" "";
+      "videos/.keep".source = builtins.toFile "keep" "";
+      "recordings/.keep".source = builtins.toFile "keep" "";
+      "wallpapers/.keep".source = builtins.toFile "keep" "";
+    };
+    
+    pointerCursor = {
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern-Classic";
+      size = 20;
+      gtk.enable = true;
+    };
+    
+    stateVersion = "24.05";    
+  };   
+
   dconf.settings = {
     "org/gnome/desktop/interface" = {
       gtk-theme = "Nordic-darker-standard-buttons";
@@ -43,26 +64,5 @@
       name = "Sans";
       size = 11;
     };
-  };
-  
-  home = {
-    username = "bzv";
-    homeDirectory = "/home/bzv";
-    file = {
-      "screenshots/.keep".source = builtins.toFile "keep" "";
-      "code/.keep".source = builtins.toFile "keep" "";
-      "videos/.keep".source = builtins.toFile "keep" "";
-      "recordings/.keep".source = builtins.toFile "keep" "";
-      "wallpapers/.keep".source = builtins.toFile "keep" "";
-    };
-    
-    pointerCursor = {
-      package = pkgs.bibata-cursors;
-      name = "Bibata-Modern-Classic";
-      size = 20;
-      gtk.enable = true;
-    };
-    
-    stateVersion = "24.05";    
   }; 
 }
