a1764e1ab581163191bd99d387aa911aa9bd4521
Adi Bozzhanov <bozzhanov.at@gmail.com>
2024-09-08 14:47:42 +0100

    Home rebuild: 2024-09-08 14:46 : id 315 -> /nix/store/hqr87qvzb5g41r5lag00yr1y4ivn2myh-home-manager-generation

--- home-manager/emacs.nix
+++ home-manager/emacs.nix
@@ -8,7 +8,13 @@
 
   programs.emacs = {
     enable = true;
-    package = pkgs.emacs;    
+    package = pkgs.emacs;
+
+    prelude = ''
+    (tool-bar-mode -1)
+    (scroll-bar-mode -1)
+    (menu-bar-mode -1)
+    '';
 
   };
 }
