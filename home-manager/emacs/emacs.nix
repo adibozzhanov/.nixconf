9557afdadb07a666955aabf1c9d2728b51e451d1
Adi Bozzhanov <bozzhanov.at@gmail.com>
2024-09-08 21:58:31 +0100

    Home rebuild: 2024-09-08 21:53 : id 329 -> /nix/store/37383sr9rpqhzxahshgml4ycdnb80152-home-manager-generation

--- home-manager/emacs/emacs.nix
+++ home-manager/emacs/emacs.nix
@@ -4,6 +4,16 @@
      enable = true;
      packageQuickstart = false;
      recommendedGcSettings = true;
-     usePackageVerbose = false;     
+     usePackageVerbose = false;
+     earlyInit = ''
+
+     ;; Remove useless UI shit
+     (tool-bar-mode -1) ; Bye tool bar
+     (scroll-bar-mode -1) ; Bye scroll bar
+     (menu-bar-mode -1) ; Bye menu bar
+
+     (require monokai-pro-theme)
+     (load-theme 'monokai-pro-theme t)
+     '';
    };
 }
