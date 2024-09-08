accdb438ed794a145ade2baaaee49584f176f733
Adi Bozzhanov <bozzhanov.at@gmail.com>
2024-09-08 21:46:56 +0100

    Home rebuild: 2024-09-08 19:47 : id 328 -> /nix/store/pbv9pc1bn9vp37vd4sgmi535l0lgca6p-home-manager-generation

--- home-manager/emacs/emacs.nix
+++ home-manager/emacs/emacs.nix
@@ -1,6 +1,13 @@
 { inputs, config, pkgs, ...}:
+
+let
+  nurr = import inputs.nur {pkgs = null; nurpkgs = pkgs};
+in
 {
-   
+  imports = [
+    nurr.repos.ryceehmdModules.emacs-init
+  ];
+  
   programs.emacs.init = {
     enable = true;
     packageQuickstart = false;
