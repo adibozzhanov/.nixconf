c95edda192759f462dc5d927047732936437da31
Adi Bozzhanov <bozzhanov.at@gmail.com>
2024-09-08 21:56:44 +0100

    Home rebuild: 2024-09-08 21:53 : id 329 -> /nix/store/37383sr9rpqhzxahshgml4ycdnb80152-home-manager-generation

--- home-manager/emacs/emacs.nix
+++ home-manager/emacs/emacs.nix
@@ -1,6 +1,9 @@
 { inputs, nur, config, pkgs, ...}:
 {  
    nur.repos.rycee.hmModules.emacs.init = {
-     
+     enable = true;
+     packageQuickstart = false;
+     recommendedGcSettings = true;
+     usePackageVerbose = false;     
    };
 }
