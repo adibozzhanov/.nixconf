6d155fff01b2502d00298c673f4f775062985cf4
Adi Bozzhanov <bozzhanov.at@gmail.com>
2024-09-08 12:50:03 +0100

    Home rebuild: 2024-09-08 12:50 : id 312 -> /nix/store/bbfbz6x0zfpmblsfb8l6wlfh3sx3fiwb-home-manager-generation

--- home-manager/hyprland/windowrules.nix
+++ home-manager/hyprland/windowrules.nix
@@ -4,6 +4,7 @@
       "suppressevent maximize, class:.*"      
       "opacity 0.9 0.8, class:^(kitty)$"
       "opacity 0.9 0.8, class:^(Spotify)$"
+      "opacity 0.9 0.8, class:^(whatsapp-for-linux)$"
       "opacity 0.95 0.8, class:^(org.telegram.desktop)$"
       "opacity 1 0.8, class:^(firefox)$"
       "float, class: ^(waypaper)$"
