385daa642143919b88c9df751efab24e5bcb0e97
Adi Bozzhanov <bozzhanov.at@gmail.com>
2024-09-08 21:45:22 +0100

    Home rebuild: 2024-09-08 19:47 : id 328 -> /nix/store/pbv9pc1bn9vp37vd4sgmi535l0lgca6p-home-manager-generation

--- home-manager/emacs/emacs.nix
+++ home-manager/emacs/emacs.nix
@@ -1,14 +1,6 @@
-{ inputs, nurNoPkgs, config, pkgs, ...}:
+{ inputs, config, pkgs, ...}:
 {
-  imports = [
-    nurNoPkgs.repos.rycee.hmModules.emacs-init
-    nurNoPkgs.myNur.repos.rycee.hmModules.emacs-notmuch
-  ];
-  
-  nixpkgs.overlays = [
-    (import inputs.emacs-overlay)
-  ];
-  
+   
   programs.emacs.init = {
     enable = true;
     packageQuickstart = false;
