87895b271b103f8964d2d0eb0a3610187c89c53e
Adi Bozzhanov <bozzhanov.at@gmail.com>
2024-09-08 21:07:40 +0100

    Home rebuild: 2024-09-08 19:47 : id 328 -> /nix/store/pbv9pc1bn9vp37vd4sgmi535l0lgca6p-home-manager-generation

--- home-manager/emacs/emacs.nix
+++ home-manager/emacs/emacs.nix
@@ -1,27 +1,21 @@
 { pkgs, ...}:
+
+let
+   use-emacs = pkgs.emacsWithPackagesFromUsePackage {
+       config = ./init.el;
+       defaultInitFile = true;
+       alwaysEnsure = true;
+   };
+in   
 {
+  programs.emacs = {
+    enable = true;
+    package = use-emacs;
+  };
 
   services.emacs = {
     enable = true;
     package = pkgs.emacs;
     defaultEditor = true;
-  };
-
-  programs.emacs.init = {
-    enable = true;
-    package = pkgs.emacs;
-    alwaysEnsure = true;
-    
-    extraConfig = ''
-    (setq package-archives nil)
-    (defalias 'yes-or-no-p 'y-or-n-p)
-    (tool-bar-mode -1)
-    (scroll-bar-mode -1)
-    (menu-bar-mode -1)
-    (add-hook 'prog-mode-hook 'display-line-numbers-mode)
-    (setq display-line-numbers-type 'relative)
-    (show-paren-mode)
-    (set-default 'truncate-lines t)
-    '';
   };
 }
