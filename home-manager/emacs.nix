b6ea2a98f889fbe61728f742389887fa61eeec8d
Adi Bozzhanov <bozzhanov.at@gmail.com>
2024-09-08 14:45:27 +0100

    Home rebuild: 2024-09-08 13:11 : id 314 -> /nix/store/d7sh6lqdala1k18hpy7x0ixii63k6jh9-home-manager-generation

---
+++ home-manager/emacs.nix
@@ -0,0 +1,18 @@
+{ pkgs, ...}:
+{
+
+  services.emacs = {
+    enable = true;
+    package = pkgs.emacs;
+  };
+
+  programs.emacs = {
+    enable = true;
+    package = pkgs.emacs;
+
+    init = {
+      enable = true;
+      recommendedGcSettings = true;
+    }
+  };
+}
