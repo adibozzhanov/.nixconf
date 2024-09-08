32ea16a01b9f17f62bd7f9913dbd5f9ef25d5ad6
Adi Bozzhanov <bozzhanov.at@gmail.com>
2024-09-08 14:58:09 +0100

    Home rebuild: 2024-09-08 14:58 : id 319 -> /nix/store/5a47fv4hhra4k050wyx8a9lqn577r5a7-home-manager-generation

--- home-manager/emacs/visual.nix
+++ home-manager/emacs/visual.nix
@@ -2,6 +2,9 @@
   programs.emacs.extraConfig = ''
     (tool-bar-mode -1)
     (scroll-bar-mode -1)
-    (menu-bar-mode -1)  
+    (menu-bar-mode -1)
+    (add-hook 'prog-mode-hook 'display-line-numbers-mode)
+    (setq display-line-numbers-type 'relative)
+
   '';
 }
