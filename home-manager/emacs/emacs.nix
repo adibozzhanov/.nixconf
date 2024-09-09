32523270afb4230ee50b08f81ad25a781099a91c
Adi Bozzhanov <bozzhanov.at@gmail.com>
2024-09-09 20:35:51 +0100

    Home rebuild: 2024-09-08 22:24 : id 332 -> /nix/store/8njv44zb11r3xkmhj2wzms0k35m9qkgi-home-manager-generation

--- home-manager/emacs/emacs.nix
+++ home-manager/emacs/emacs.nix
@@ -3,18 +3,11 @@
   services.emacs = {
     enable = true;
   };
-  
+
+  home.file.".emacs.d".source = ./.emacs.d;
+
   programs.emacs = {
     enable = true;
     package = pkgs.emacs;
-    extraConfig = ''
-
-    (tool-bar-mode -1) ; Bye tool bar
-    (scroll-bar-mode -1) ; Bye scroll bar
-    (menu-bar-mode -1) ; Bye menu bar
-
-    (add-hook 'prog-mode-hook 'display-line-numbers-mode)
-    (setq display-line-numbers-type 'relative)
-    '';
   };
 }
