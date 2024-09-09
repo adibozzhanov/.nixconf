e49aa923a00053456c231ea46dafa59270262a8c
Adi Bozzhanov <bozzhanov.at@gmail.com>
2024-09-09 21:40:52 +0100

    Home rebuild: 2024-09-09 21:40 : id 342 -> /nix/store/wk0x7g93brhss1z5kfa30k2rs6zr6m1a-home-manager-generation

--- home-manager/hyprland/windowrules.nix
+++ home-manager/hyprland/windowrules.nix
@@ -1,10 +1,11 @@
 {
   wayland.windowManager.hyprland.settings = {
-    windowrulev2 = [      
-      "suppressevent maximize, class:.*"      
+    windowrulev2 = [
+      "suppressevent maximize, class:.*"
       "opacity 0.9 0.8, class:^(kitty)$"
       "opacity 0.9 0.8, class:^(Spotify)$"
       "opacity 0.9 0.8, class:^(whatsapp-for-linux)$"
+      "opacity 0.9 0.8, class:^(Emacs)$"
       "opacity 0.95 0.8, class:^(org.telegram.desktop)$"
       "opacity 1 0.8, class:^(firefox)$"
       "float, class: ^(waypaper)$"
