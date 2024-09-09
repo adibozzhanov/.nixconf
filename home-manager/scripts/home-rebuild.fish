f147962f2839c63257c903af29355a40a0d24b0f
Adi Bozzhanov <bozzhanov.at@gmail.com>
2024-09-09 20:56:13 +0100

    Home rebuild: 2024-09-09 20:55 : id 335 -> /nix/store/j3kvmbp1pakjjx364i8ldqmb0gapr5fz-home-manager-generation

--- home-manager/scripts/home-rebuild.fish
+++ home-manager/scripts/home-rebuild.fish
@@ -2,6 +2,6 @@ pushd ~/.nixconf
 git diff --minimal -U0
 echo "Home manager rebuilding..."
 git add -A
-home-manager switch --flake ./
+home-manager switch --flake ".?submodules=1"
 git commit -am "Home rebuild: $(home-manager generations | head -n 1)"
 popd
--- home-manager/scripts/nix-rebuild.fish
+++ home-manager/scripts/nix-rebuild.fish
@@ -2,7 +2,7 @@ pushd ~/.nixconf
 git diff --minimal -U0
 git add -A
 echo "NixOS Rebuilding..."
-sudo nixos-rebuild switch --flake ./
+sudo nixos-rebuild switch --flake ".?submodules=1"
 git commit -am "NixOs rebuild: $(nixos-rebuild list-generations | grep current)"
 popd
 
