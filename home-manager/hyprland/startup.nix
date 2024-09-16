197e1fbc9dd85fdeacf11c2fede7758d5c0c6704
Adi Bozzhanov <bozzhanov.at@gmail.com>
2024-09-16 16:38:56 +0100

    Home rebuild: 2024-09-16 16:38 : id 391 -> /nix/store/kdqh3xj1yqy2i1idc4z37za607fisch9-home-manager-generation

--- home-manager/hyprland/startup.nix
+++ home-manager/hyprland/startup.nix
@@ -1,11 +1,17 @@
 {
   wayland.windowManager.hyprland.settings = {
+    env = [
+      "XDG_CURRENT_DESKTOP,Hyprland"
+    ];
     exec-once = [
+      "dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
+      "dbus-update-activation-environment --systemd --all"
+      "systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
       "swaync"
       "nm-applet"
       "systemctl --user start plasma-polkit-agent"
       "chmod +x ~/.nixconf/home-manager/scripts/*"
       "~/.nixconf/home-manager/scripts/wallpaper.sh"
-    ];    
+    ];
   };
 }
